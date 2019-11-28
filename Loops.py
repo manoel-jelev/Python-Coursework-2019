efa58bcb0a251000bb858755dfb2e69aebb382a5
manoel-jelev <58301477+manoel-jelev@users.noreply.github.com>
2019-11-28 18:19:47 +0200

    hacker_rank_challenges

---
+++ Loops.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+
+for numb in range(0,n):
+    print(numb*numb)
---
+++ Python_If-Else.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n%2!=0 :
+    print("Weird")
+else:
+    if 2<=n and n<=5 :
+        print("Not Weird")
+    if 6<=n and n<=20:
+        print("Weird")
+    if n>20 :
+        print("Not Weird")
+
